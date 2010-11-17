3acb090f9c94fdf35668de33b726c681062e25c9
Felix Hummel <hg@felixhummel.de>
2010-11-17 21:07:43 +0100

    default evently call (string vs ddoc)

--- evently/tasks/recent/selectors/ul/_changes/selectors/a[href=#reply]/click.js
+++ evently/tasks/recent/selectors/ul/_changes/selectors/a[href=#reply]/click.js
@@ -1,6 +1,6 @@
 function() {
   var li = $(this).parents("li");
   var app = $$(this).app;
-  $("div.replies",li).evently(app.ddoc.evently.replies, app);
+  $("div.replies",li).evently("replies", app);
   return false;
 }
