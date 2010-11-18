d3ddec0af6863e284d62594f89d3f8f387e9e579
Felix Hummel <hg@felixhummel.de>
2010-11-18 17:26:12 +0100

    path display is nice

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -1,4 +1,7 @@
 $.couch.app(function(app) {  
   $("#content").evently("issues", app);
+  $.pathbinder.onChange(function(path) {
+    $("#current-path").text(path);
+  });
   $.pathbinder.begin("/list");
 });
