1b1f84b92cf72b82b43ecf422007ce4f4634bbbf
Felix Hummel <hg@felixhummel.de>
2010-11-23 10:28:09 +0100

    issue submit done

--- evently/issues/new/selectors/form/submit.js
+++ evently/issues/new/selectors/form/submit.js
@@ -1,5 +1,13 @@
 function() {
   var form = $(this);
-  $.log(form);
+  var fdoc = form.serializeObject();
+  fdoc.created_at = new Date();
+  fdoc.profile = $$("#profile").profile;
+  fdoc.type = 'issue';
+  $$(this).app.db.saveDoc(fdoc, {
+    success : function() {
+      form[0].reset();
+    }
+  });
   return false;
 }
