2b44170d4d9ebf3354cf012a44dff30964c75fd9
Felix Hummel <hg@felixhummel.de>
2010-11-24 13:21:05 +0100

    fixed reply not showing

--- evently/issues/show/selectors/form/submit.js
+++ evently/issues/show/selectors/form/submit.js
@@ -4,7 +4,7 @@ function() {
   fdoc.created_at = new Date();
   fdoc.profile = $$("#profile").profile;
   fdoc.type = 'reply';
-  fdoc.issue = $('.id').text();
+  fdoc.issue = $('#id').text();
   $$(this).app.db.saveDoc(fdoc, {
     success : function() {
       form[0].reset();
