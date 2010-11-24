919a1eae0ad189d3a69abea2656ddb1da02e704e
Felix Hummel <hg@felixhummel.de>
2010-11-24 21:05:03 +0100

    select status

--- evently/issues/show/after.js
+++ evently/issues/show/after.js
@@ -1,4 +1,6 @@
 function(resp) {
   // remember complete doc in current
   $$("#content").current = resp.rows[0].value;
+  var status = $$("#content").current.status;
+  $("select", this).val(status);
 };
