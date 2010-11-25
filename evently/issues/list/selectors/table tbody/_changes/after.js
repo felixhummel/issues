cc3699e3a8542dea9d8d6a84885dbac7a9dae5db
Felix Hummel <hg@felixhummel.de>
2010-11-25 16:39:59 +0100

    cosmetic changes (list table style, new issue button)

---
+++ evently/issues/list/selectors/table tbody/_changes/after.js
@@ -0,0 +1,10 @@
+function(resp, e) {
+  // XXX hack. must be a better way to get new row...
+  var new_row = $(this).children().last();
+  new_row.click(function() {
+    var link = $(this).attr('link');
+    window.location = link;
+  });
+  new_row.mouseover(function(){$(this).addClass("over");}).mouseout(function(){$(this).removeClass("over");});
+  $("tbody tr:even").addClass("even");
+}
--- evently/issues/show/data.js
+++ evently/issues/show/data.js
@@ -1,5 +1,6 @@
 function(resp, cb, params) {
   var doc = resp.rows[0].value;
+  doc.created_at = $.prettyDate(doc.created_at);
   doc = $.extend(doc, {
     author: doc.profile && doc.profile.nickname || ""
   });
