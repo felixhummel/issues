4c516f4adde5e5fb856ce31b246ddc2c0ee0c928
Felix Hummel <hg@felixhummel.de>
2010-12-18 17:04:52 +0100

    evently ist eine sackgasse. problem: komme nicht an neues Element von newRows

---
+++ evently/issues/list/selectors/#cards/_changes/data.js
@@ -0,0 +1,10 @@
+function(r) {
+  var doc = r.value;
+  return {
+    text: doc.text,
+    created_at: $.prettyDate2(doc.created_at),
+    id: doc._id,
+    status: doc.status,
+    doc: JSON.stringify(doc)
+  };
+}
