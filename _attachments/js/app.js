70ee7d1000619e15c6f48c97c5218b0a06e7052d
Felix Hummel <hg@felixhummel.de>
2010-12-18 15:26:19 +0100

    evently new creates valid docs now

--- _attachments/js/app.js
+++ _attachments/js/app.js
@@ -1,4 +1,5 @@
 $.couch.app(function(app) {  
+  // those lines give us a user object in $$("#profile").profile
   $("#profile").evently("profile", app);
   $("#account").evently("account", app);
   $.evently.connect($("#account"), $("#profile"), ["loggedIn", "loggedOut"]);
