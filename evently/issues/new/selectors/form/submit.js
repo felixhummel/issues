a0b70c48f43d12eddbd0dc4db7a356aa27e5fedf
Felix Hummel <hg@felixhummel.de>
2010-12-18 16:22:37 +0100

    evently/new sets status=open

--- evently/issues/new/selectors/form/submit.js
+++ evently/issues/new/selectors/form/submit.js
@@ -4,6 +4,7 @@ function() {
   fdoc.created_at = new Date();
   fdoc.profile = $$("#profile").profile;
   fdoc.type = 'issue';
+  fdoc.status = 'open';
   $$(this).app.db.saveDoc(fdoc, {
     success : function() {
       form[0].reset();
