490b0aa70629832d57ee048cacb02618d5c0bbd6
Felix Hummel <hg@felixhummel.de>
2011-01-03 16:54:59 +0100

    minor refactorings in edit

--- shows/edit.js
+++ shows/edit.js
@@ -1,16 +1,24 @@
-function(doc, req) {  
+function(doc, req) {
   var ddoc = this;
   var Mustache = require("lib/mustache");
   var path = require("vendor/couchapp/lib/path").init(req);
 
+  // create empty objects for partials
   var data = {
     scripts: {},
     issue: {}
   };
-  if (!doc) {
-  } else {
+  if (doc) {
+    // remove _revisions - we don't need them and would get update conflicts
+    doc._revisions && delete doc._revisions;
+    // simply stringify the whole document
     data.doc = JSON.stringify(doc);
     return Mustache.to_html(ddoc.templates.edit, data, ddoc.templates.partials);
+  } else {
+    data = {
+      id: req.id
+    }
+    return Mustache.to_html(ddoc.templates.err404, data);
   }
-
 }
+
